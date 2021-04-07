798209482c57bdefed255e4842f0dcb06c1ecfe3
Lauriane <lauriane.haydari@gmail.com>
2021-04-07 18:37:41 +0200

    init project

---
+++ Reciplease/App/Home/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Reciplease
+//
+//  Created by Lau on 07/04/2021.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+}
+
---
+++ Reciplease/App/Home/HomeViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  HomeViewModel.swift
+//  Reciplease
+//
+//  Created by Lau on 07/04/2021.
+//
+
+import Foundation
+
+protocol HomeViewModelDelegate {
+    
+}
+
+class HomeViewModel {
+    
+}
